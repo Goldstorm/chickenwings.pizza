a4c3c948f26bf2d32c81a49563056c56d18640a1
nickg <nickagoldstein@gmail.com>
2018-12-13 18:34:49 -0500

    Followed the djangorestframework api tutorial.

--- nick/settings.py
+++ nick/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rlmwt7nyl8yxf)na+6*o(k)1b8@-h@wy=e01d=^32e24&x1-w2'
 DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['nickgoldstein.com', '10.0.0.18']
+ALLOWED_HOSTS = ['nickgoldstein.com', '10.0.0.18', '127.0.0.1']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = 'nick.urls'
 
